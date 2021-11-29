4b6374b988ac466d80fbea08429cf09a2b8c7af5
biancajanson <biancajanson@gmail.com>
2021-11-29 16:49:06 +0100

    Update assignments.js

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -2,3 +2,16 @@
 
 // LECTURE: FUNCTIONS
 
+function describeCountry(country, population, capitalCity) {
+    const description = `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+    return description;
+}
+
+const netherlands = describeCountry("The Netherlands", 17, "Amsterdam");
+console.log(netherlands);
+
+const finland = describeCountry("Finland", 6, "Helsinki");
+console.log(finland);
+
+const ireland = describeCountry("Ireland", 5, "Dublin");
+console.log(ireland);
