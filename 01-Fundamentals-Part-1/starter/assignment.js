e68f4e26ca277e549e0e96609b19816c06ba9f32
biancajanson <biancajanson@gmail.com>
2021-11-29 10:56:04 +0100

    assignment TERNARY toegevoegd

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -95,25 +95,32 @@
 
 // LECTURE: THE SWITCH STATEMENT
 
-const language = 'dutch'
+// const language = 'dutch'
 
-switch (language) {
-    case 'chinese':
-    case 'mandarin':
-        console.log('MOST number of native speakers')
-        break;
-    case 'spanish':
-        console.log('2nd place in number of native speakers')
-        break;
-    case 'english':
-        console.log('3rd place')
-        break;
-    case 'hindi':
-        console.log('Number 4')
-        break;
-    case 'arabic':
-        console.log('5th most spoken language')
-        break;
-    default:
-        console.log('Great language too :D') 
-}
+// switch (language) {
+//     case 'chinese':
+//     case 'mandarin':
+//         console.log('MOST number of native speakers')
+//         break;
+//     case 'spanish':
+//         console.log('2nd place in number of native speakers')
+//         break;
+//     case 'english':
+//         console.log('3rd place')
+//         break;
+//     case 'hindi':
+//         console.log('Number 4')
+//         break;
+//     case 'arabic':
+//         console.log('5th most spoken language')
+//         break;
+//     default:
+//         console.log('Great language too :D') 
+// }
+
+// LECTURE: THE CONDITIONAL (TERNARY) OPERATOR
+
+const population = 17
+const averagePopulation = population > 33 ? 'above' : 'below';
+console.log(averagePopulation)
+console.log(`The Netherlands population is ${population > 33 ? 'above' : 'below'} average.`);
