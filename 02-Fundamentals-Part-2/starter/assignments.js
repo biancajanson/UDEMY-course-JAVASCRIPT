bbeff90d2d5c45547ccc0796726ce8cc2976243f
biancajanson <biancajanson@gmail.com>
2021-11-30 09:44:48 +0100

    Assignment functions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -2,16 +2,36 @@
 
 // LECTURE: FUNCTIONS
 
-function describeCountry(country, population, capitalCity) {
-    const description = `${country} has ${population} million people and its capital city is ${capitalCity}.`;
-    return description;
+// function describeCountry(country, population, capitalCity) {
+//     const description = `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+//     return description;
+// }
+
+// const netherlands = describeCountry("The Netherlands", 17, "Amsterdam");
+// console.log(netherlands);
+
+// const finland = describeCountry("Finland", 6, "Helsinki");
+// console.log(finland);
+
+// const ireland = describeCountry("Ireland", 5, "Dublin");
+// console.log(ireland);
+
+// LECTURE: FUNCTION DECLARATIONS VS. EXPRESSIONS
+
+// DECLARATION
+function percentageOfWorld1(population) {
+    return (population/7900)*100;
 }
+const china1 = percentageOfWorld1(1441);
+const netherlands1 = percentageOfWorld1(16);
+const germany1 = percentageOfWorld1(83);
+console.log(china1, netherlands1, germany1)
 
-const netherlands = describeCountry("The Netherlands", 17, "Amsterdam");
-console.log(netherlands);
-
-const finland = describeCountry("Finland", 6, "Helsinki");
-console.log(finland);
-
-const ireland = describeCountry("Ireland", 5, "Dublin");
-console.log(ireland);
+//EXPRESSION
+const percentageOfWorld2 = function (population) {
+    return (population/7900)*100;
+}
+const china2 = percentageOfWorld1(1441);
+const netherlands2 = percentageOfWorld1(16);
+const germany2 = percentageOfWorld1(83);
+console.log(china2, netherlands2, germany2)
