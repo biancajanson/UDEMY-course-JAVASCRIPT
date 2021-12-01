68e7718fd010433355faf76bd3b71e4022a3275f
biancajanson <biancajanson@gmail.com>
2021-12-01 11:32:47 +0100

    coding challenge 2

--- 02-Fundamentals-Part-2/starter/codingchallenges.js
+++ 02-Fundamentals-Part-2/starter/codingchallenges.js
@@ -26,3 +26,14 @@
 // scoreKoalas = calcAverage(23, 34, 27);
 
 // checkWinner(scoreDolphins, scoreKoalas);
+
+
+// CODING CHALLENGE 2
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2; 
+}
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(bills,tips);
