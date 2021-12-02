cbc636eab5bbe28141fbfbf082bcfd4b873b384e
biancajanson <biancajanson@gmail.com>
2021-12-02 13:12:39 +0100

    Update assignments.js

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -120,4 +120,4 @@ const jonas = {
 }
 
 console.log(jonas.firstName);
-console.log(jonas['firstName']+ ` has ` +(jonas.friends.length) + ` friends, and his best friend is called ` + jonas.friends[0])
+console.log(`${jonas['firstName']} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
